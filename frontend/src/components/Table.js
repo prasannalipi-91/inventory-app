f8545daa3eb94faecb76d16128613adf4eb1326b
Prasanna Kotuwegedara <prasanna.pdkk@gmail.com>
2024-01-30 09:43:46 +0300

    download csv added

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Table, Container } from 'react-bootstrap';
+import { Table, Container, Button } from 'react-bootstrap';
 
 const Tablecomponent = ({ item }) => {
+  const downloadCsv = () => {
+    const csvData =
+      'data:text/csv;charset=utf-8,' +
+      item
+        .map((element) =>
+          Object.values(element)
+            .map((value) => `"${String(value)}"`.replace(/\n/g, ''))
+            .join(','),
+        )
+        .join('\n');
+    // Create a link and trigger the download
+    const encodedUri = encodeURI(csvData);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', 'inventory.csv');
+    document.body.appendChild(link);
+    link.click();
+  };
   return (
     <Container className="mt-2">
       <Table striped bordered hover>
+        <Container className="mb-2" style={{ textAlign: 'left' }}>
+          <Button variant="secondary" size="sm" onClick={downloadCsv}>
+            export to csv
+          </Button>
+        </Container>
         <tbody className="mt-0">
           {item.map((element, index) => (
             <tr>
