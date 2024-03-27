ab37eb0c0a66f957ea9e68aefc75b653f8d3167a
Prasanna Kotuwegedara <prasanna.pdkk@gmail.com>
2024-03-27 12:22:50 +0300

    Finalized_inv_and_reach

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ import Header from './components/Header.js';
 //import ImageCard from './components/ImageCard.js';
 //import Tablecomponent from './components/Table.js';
 import CustomizedTables from './components/muitable.js';
+import CustomizedTables1 from './components/muistable.js';
 import Selections from './components/Searchui.js';
+import { appBarClasses } from '@mui/material';
 
 function App() {
   const [word, setWord] = useState('');
   const [items, setItems] = useState([]);
+  const [hcitems, setHcitems] = useState([]);
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
-    console.log(word);
     fetch(`http://127.0.0.1:5000/api/data`)
       .then((res) => res.json())
       .then((data) => {
@@ -23,8 +25,23 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
-    setWord('');
+    setWord('Inventory');
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://127.0.0.1:5000/api/hc_switch_status`)
+      .then((res) => res.json())
+      .then((data) => {
+        setHcitems(data, ...hcitems);
+        console.log(hcitems);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord('Reachability');
+  };
+
   return (
     <div className="App">
       <Header title="Inventory Tool" />
@@ -32,11 +49,13 @@ function App() {
         word={word}
         setWord={setWord}
         handleSubmit={handleSearchSubmit}
+        handleSubmit1={handleSubmit}
       />
-      {items.length > 0 ? (
+      {items.length > 0 && word === 'Inventory' && (
         <CustomizedTables item={items} />
-      ) : (
-        <p>Please Select the Feature</p>
+      )}
+      {hcitems.length > 0 && word === 'Reachability' && (
+        <CustomizedTables1 item={hcitems} />
       )}
     </div>
   );
--- frontend/src/components/Searchui.js
+++ frontend/src/components/Searchui.js
@@ -3,15 +3,16 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import { useState } from 'react';
 
 
 
-export default function Selections( {handleSubmit} ) {
+export default function Selections( {handleSubmit, handleSubmit1} ) {
   
   const buttons = [
     <Button key="one" onClick={handleSubmit}>Inventory</Button>,
     <Button key="two">Topology</Button>,
-    <Button key="three">Device Status</Button>,
+    <Button key="three" onClick={handleSubmit1}>Device Status</Button>,
   ];
 
   return (
